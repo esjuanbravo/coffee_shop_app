7bf2a0f884ea09480daf34b3a74e9874693fb545
esjuanbravo <esjuanbravo12@gmail.com>
2025-05-29 11:11:18 -0400

    Refactor logout view and improve success URL handling in login view

--- user/views.py
+++ user/views.py
@@ -4,14 +4,18 @@ from django.contrib.auth.views import LoginView
 from .forms import LoginForm
 from django.urls import reverse_lazy
 
+
 class CustomLoginView(LoginView):
     form_class = LoginForm
     template_name = 'user/login_user.html'
     success_url = reverse_lazy('home')
 
     def get_success_url(self):
+        redirect_to = self.get_redirect_url()
+        if redirect_to:
+            return redirect_to
         return self.success_url
 
-    def logout_view(request):
-        logout(request)
-        return redirect('home')
+def logout_view(request):
+    logout(request)
+    return redirect('home')
