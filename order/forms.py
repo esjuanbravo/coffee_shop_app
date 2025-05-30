bd232e0a5c09140b6966be4bc888a7ded6568c84
esjuanbravo <esjuanbravo12@gmail.com>
2025-05-29 23:31:37 -0400

    Add order product creation functionality and update order views and templates

---
+++ order/forms.py
@@ -0,0 +1,9 @@
+from django.forms import  ModelForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import OrderProduct
+
+class OrderProductForm(ModelForm):
+    class Meta:
+        model= OrderProduct
+        fields = ['product']
+        
--- order/urls.py
+++ order/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import MyOrder
+from .views import MyOrder, CreateOrderProductView
+
+
 urlpatterns=[
-    path('orden/', MyOrder.as_view() , name='orders')
+    path('orden/', MyOrder.as_view() , name='orders'),
+    path('agregar-pedido/', CreateOrderProductView.as_view(), name='add_product')
 ]
